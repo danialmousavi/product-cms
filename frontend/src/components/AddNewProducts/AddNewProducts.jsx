650d4ac1ee2f8ca67131bf175eb6e1b8978bcf28
danialmousavi <danialmoosavi69@gmail.com>
2025-04-07 08:18:21 +0330

    add new product and fix database

--- frontend/src/components/AddNewProducts/AddNewProducts.jsx
+++ frontend/src/components/AddNewProducts/AddNewProducts.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './AddNewProducts.css'
 import { MdOutlineAttachMoney } from "react-icons/md";
 import { IoImageOutline } from "react-icons/io5";
@@ -9,6 +9,47 @@ import { LuMessageCircleHeart } from "react-icons/lu";
 import { FaChartLine } from "react-icons/fa6";
 
 export default function AddNewProducts() {
+const [newProductTitle,SetNewProductTitle]=useState('');
+const [newProductPrice,SetNewProductPrice]=useState('');
+const [newProductCount,SetNewProductCount]=useState('');
+const [newProductImg,SetNewProductImg]=useState('');
+const [newProductPopularity,SetNewProductPopularity]=useState('');
+const [newProductSale,SetNewProductSale]=useState('');
+const [newProductColors,SetNewProductColors]=useState('');
+
+const newProductObj={
+    title:newProductTitle,
+    price:newProductPrice,
+    count:newProductCount,
+    img:newProductImg,
+    popularity:newProductPopularity,
+    sale:newProductSale,
+    colors:newProductColors
+}
+const addNewProduct = (e) => {
+    e.preventDefault();
+
+    fetch('http://localhost:3000/api/products', {
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json',
+        },
+        body: JSON.stringify(newProductObj)
+    })
+    .then(async res => {
+        if (res.ok) {
+            const data = await res.json();
+            console.log("Product added successfully:", data);
+            // Optionally update UI or state here
+        } else {
+            const error = await res.text(); // or res.json() if your server sends JSON errors
+            console.error("Failed to add product:", error);
+        }
+    })
+    .catch(err => {
+        console.error("Network or server error:", err);
+    });
+};
   return (
     <>
     <div className='products-main'>
@@ -17,34 +58,34 @@ export default function AddNewProducts() {
             <div className='add-products-form-wrap'>
                 <div className='add-products-from-group'>
                     <MdDriveFileRenameOutline className='products-input-icon'/>
-                    <input type="text" placeholder='اسم محصول را بنویسید' className='add-products-input' />
+                    <input type="text" placeholder='اسم محصول را بنویسید' className='add-products-input'onChange={(e)=>SetNewProductTitle(e.target.value)} value={newProductTitle}/>
                 </div>
                 <div className='add-products-from-group'>
                     <MdOutlineAttachMoney className='products-input-icon'/>
-                    <input type="text" placeholder='قیمت محصول را بنویسید' className='add-products-input' />
+                    <input type="text" placeholder='قیمت محصول را بنویسید' className='add-products-input'onChange={(e)=>SetNewProductPrice(e.target.value)} value={newProductPrice}/>
                 </div>
                 <div className='add-products-from-group'>
                     <IoBagOutline className='products-input-icon'/>
-                    <input type="text" placeholder='موجودی محصول را بنویسید' className='add-products-input' />
+                    <input type="text" placeholder='موجودی محصول را بنویسید' className='add-products-input'onChange={(e)=>SetNewProductCount(e.target.value)} value={newProductCount}/>
                 </div>
                 <div className='add-products-from-group'>
                     <IoImageOutline className='products-input-icon'/>
-                    <input type="text" placeholder='آدرس عکس محصول را بنویسید' className='add-products-input' />
+                    <input type="text" placeholder='آدرس عکس محصول را بنویسید' className='add-products-input' onChange={(e)=>SetNewProductImg(e.target.value)} value={newProductImg} />
                 </div>
                 <div className='add-products-from-group'>
                     <LuMessageCircleHeart className='products-input-icon'/>
-                    <input type="text" placeholder='میزان محبوبیت محصول را بنویسید' className='add-products-input' />
+                    <input type="text" placeholder='میزان محبوبیت محصول را بنویسید' className='add-products-input' onChange={(e)=>SetNewProductPopularity(e.target.value)} value={newProductPopularity}/>
                 </div>
                 <div className='add-products-from-group'>
                     <FaChartLine className='products-input-icon'/>
-                    <input type="text" placeholder='میزان فروش محصول را بنویسید' className='add-products-input' />
+                    <input type="text" placeholder='میزان فروش محصول را بنویسید' className='add-products-input'onChange={(e)=>SetNewProductSale(e.target.value)} value={newProductSale}/>
                 </div>
                 <div className='add-products-from-group'>
                     <LuPaintbrush className='products-input-icon'/>
-                    <input type="text" placeholder='تعداد رنگ بندی محصول را بنویسید' className='add-products-input' />
+                    <input type="text" placeholder='تعداد رنگ بندی محصول را بنویسید' className='add-products-input'onChange={(e)=>SetNewProductColors(e.target.value)} value={newProductColors}/>
                 </div>
             </div>
-            <button className='add-product-submit'>اضافه کردن محصول</button>
+            <button className='add-product-submit' onClick={addNewProduct}>اضافه کردن محصول</button>
 
         </form>
     </div>
