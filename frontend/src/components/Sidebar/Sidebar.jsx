a8a5de6728c3d6a7ca5fa54214dbdd9298bbca23
danialmousavi <danialmoosavi69@gmail.com>
2025-04-10 23:31:20 +0330

    fix sidebar

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -6,49 +6,37 @@ import { BiCommentDetail } from "react-icons/bi";
 import { TbUsersGroup } from "react-icons/tb";
 import { IoBagHandleOutline } from "react-icons/io5";
 import { TbSquareRoundedPercentage } from "react-icons/tb";
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 export default function Sidebar() {
   return (
     <>
         <div className='sidebar'>
             <h1 className="sidebar-title">به داشبورد خود خوش آمدید</h1>
             <ul className='sidebar-links'>
-                <li >
-                    <Link to='/'>
+                    <NavLink to='/'>
                         <IoHomeOutline className='icon'/>
                         صفحه اصلی
-                    </Link>
-                </li>
-                <li className='active'>
-                    <Link to='/products'>
+                    </NavLink>
+                    <NavLink to='/products'>
                         <MdProductionQuantityLimits className='icon'/>
                         محصولات
-                        </Link>
-                </li>
-                <li>
-                    <Link to='/comments'>
+                        </NavLink>
+                    <NavLink to='/comments'>
                         <BiCommentDetail className='icon'/>
                         کامنت ها
-                    </Link>
-                </li>
-                <li>
-                    <Link to='/users'>
+                    </NavLink>
+                    <NavLink to='/users'>
                         <TbUsersGroup className='icon'/>
                         کاربران
-                    </Link>
-                </li>
-                <li>
-                    <Link to='/orders'>
+                    </NavLink>
+                    <NavLink to='/orders'>
                         <IoBagHandleOutline className='icon'/>
                         سفارشات
-                    </Link>
-                </li>
-                <li>
-                    <Link to='/offs'>
+                    </NavLink>
+                    <NavLink to='/offs'>
                         <TbSquareRoundedPercentage className='icon'/>
                         تخفیف ها
-                    </Link>
-                </li>
+                    </NavLink>
             </ul>
         </div>
     </>
